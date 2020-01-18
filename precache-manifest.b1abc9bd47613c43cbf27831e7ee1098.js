282b4bf5ad6130960d59201ebf95612a287601a0
Marina <marfen24@i.ua>
2020-01-18 11:46:50 +0200

    Updates

---
+++ precache-manifest.b1abc9bd47613c43cbf27831e7ee1098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d046100e72cf652a2d6063ee35c89a27",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "afe12abf4fa5f021418c",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.83670195.chunk.css"
+  },
+  {
+    "revision": "b1330d740cf7a75eca17",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.9d28df95.chunk.css"
+  },
+  {
+    "revision": "afe12abf4fa5f021418c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.24cc1352.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.24cc1352.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1330d740cf7a75eca17",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f86732d9.chunk.js"
+  },
+  {
+    "revision": "063097635f1add7d9f2c",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.d3f3cb3b.js"
+  }
+]);
